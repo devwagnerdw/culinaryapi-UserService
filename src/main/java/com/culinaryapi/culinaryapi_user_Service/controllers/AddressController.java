22dbd7af5c5cdb481dcee8bd1493f7a8bb1dd733
vagne <vagnerdw1@gmail.com>
2025-01-29 15:16:50 -0300

    Refatora nomes de classes e métodos no controlador de endereços para melhorar a clareza

--- src/main/java/com/culinaryapi/culinaryapi_user_Service/controllers/AddressController.java
+++ src/main/java/com/culinaryapi/culinaryapi_user_Service/controllers/AddressController.java
@@ -3,34 +3,31 @@ package com.culinaryapi.culinaryapi_user_Service.controllers;
 import com.culinaryapi.culinaryapi_user_Service.dtos.AddressDto;
 import com.culinaryapi.culinaryapi_user_Service.model.AddressModel;
 import com.culinaryapi.culinaryapi_user_Service.model.UserModel;
-import com.culinaryapi.culinaryapi_user_Service.repositories.AddressRepository;
 import com.culinaryapi.culinaryapi_user_Service.services.AddressService;
 import com.culinaryapi.culinaryapi_user_Service.services.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/addresses")
-public class AdressController {
+public class AddressController {
 
     private final UserService userService;
     private final AddressService addressService;
 
-    public AdressController(UserService userService, AddressService addressService) {
+    public AddressController(UserService userService, AddressService addressService) {
         this.userService = userService;
         this.addressService = addressService;
     }
 
     @PostMapping("/")
-    public ResponseEntity<Object> registerAddress(@RequestBody @Validated AddressDto addressDto){
+    public ResponseEntity<Object> createAddress(@RequestBody @Validated AddressDto addressDto){
 
         Optional<UserModel> optionalUserModel= userService.findById(addressDto.getId());
         if (optionalUserModel.isEmpty()){
@@ -42,6 +39,14 @@ public class AdressController {
             addressService.save(addressModel);
             return ResponseEntity.status(HttpStatus.CREATED).body(addressModel);
         }
+    }
 
+    @GetMapping("/{userId}")
+    public ResponseEntity<Object> getUserWithAddresses(@PathVariable(value = "userId") UUID userId) {
+        Optional<UserModel> userModelOptional = userService.findById(userId);
+        if (userModelOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
     }
 }
--- src/main/java/com/culinaryapi/culinaryapi_user_Service/repositories/UserRepository.java
+++ src/main/java/com/culinaryapi/culinaryapi_user_Service/repositories/UserRepository.java
@@ -1,10 +1,14 @@
 package com.culinaryapi.culinaryapi_user_Service.repositories;
 
 import com.culinaryapi.culinaryapi_user_Service.model.UserModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserModel,UUID> {
 
+    @EntityGraph(attributePaths = "addresses", type = EntityGraph.EntityGraphType.FETCH)
+    Optional<UserModel> findById(UUID userId);
 }
